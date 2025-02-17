a4913677b21664998504f1225eb9322bef3fde0d
CYROSE JAMES HERNALIN <2201038@cc.usls.edu>
2025-02-17 14:16:47 +0800

    Initial commit - Laravel Project

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello Laravel!';
+})->name('greeting');
+
+Route::get('/show-greeting', [WelcomeController::class, 'showGreeting']);
+Route::get('/redirect-greeting', [WelcomeController::class, 'redirectGreeting']);
+
+Route::get('/user/{name?}', function ($name = null) {
+    if ($name) {
+        return 'Your name is ' . $name;
+    } else {
+        return 'No name provided';
+    }
+});
